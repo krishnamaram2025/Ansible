cc642565b6a5124dd8af9f35798b5ce8c97f977d
krishnamaram2025 <112494492+krishnamaram2025@users.noreply.github.com>
2023-05-15 13:49:03 +0530

    Create mysql_python.py

---
+++ mysql/mysql_python.py
@@ -0,0 +1,31 @@
+import mysql.connector
+
+# Establish a connection to the MySQL database
+cnx = mysql.connector.connect(
+    host="54.208.14.11",
+    user="ot39",
+    password="NT27",
+    database="bible"
+)
+
+# Create a cursor to interact with the database
+cursor = cnx.cursor()
+
+# Execute a SELECT query
+query = "SELECT * FROM israel_tribes"
+cursor.execute(query)
+
+# Fetch all records from the result set
+records = cursor.fetchall()
+
+# Process the fetched records
+for record in records:
+    # Access individual columns using indexing or column names
+    column1 = record[0]
+    column2 = record[1]
+    # ... continue accessing other columns as needed
+    print(column1, column2)
+
+# Close the cursor and database connection
+cursor.close()
+cnx.close()
