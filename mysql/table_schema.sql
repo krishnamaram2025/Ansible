69c5abaaeeda0ab19a2501e3f37ec3777bae8f26
Cloud User <centos@ip-10-0-1-97.ec2.internal>
2023-05-15 07:19:06 +0000

    added schena

--- mysql/table_schema.sql
+++ mysql/table_schema.sql
@@ -1,1 +1,10 @@
+create database bible_rd;
 
+CREATE TABLE israel_tribes (
+  id INT PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  meaning VARCHAR(255),
+  founding_father VARCHAR(50),
+  land VARCHAR(255),
+  notable_figures VARCHAR(255)
+);
